9c5d2555700e9336be17ba6a746ec171f2bd1c3e
Jhony Vidal <mrjhonyvidal@gmail.com>
2022-01-09 21:33:20 +0000

    Add CustomErrorHanlder Abstract class and some implementations for Auth Sign up

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
     err: Error, 
@@ -6,9 +9,12 @@ export const errorHandler = (
     res: Response, 
     next: NextFunction
 ) => {
-    console.log('Description of the error', err);
+    
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }
 
     res.status(400).send({
-        message: 'Something went wrong'
+        errors: [{message: 'Something went wrong' }]
     });
 };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -11,18 +13,19 @@ router.post('/api/users/signup/:id', [
         .trim()
         .isLength({ min:4, max: 20 })
         .withMessage('Password must be between 4 and 20 characters')
-],(req: Request, res: Response) => {
+], async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        throw new Error('Invalid email or password');
+        // Throw exception
+        next(new RequestValidationError(errors.array()));
     }
 
     const { email, password } = req.body;
 
     console.log('Creating a user...');
     // Temporarily - test our new ErrorHandler middleware
-    throw new Error('Error connecting to database');
+    throw new DatabaseConnectionError();
 
     res.send({});
 });
